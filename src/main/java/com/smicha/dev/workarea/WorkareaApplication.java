5951ebb10227657bada7d36211ecdd9c6618a6a3
Sajeeve M <smicha@smicha-macBookAir.local>
2020-05-11 20:49:23 -0700

    first commit

---
+++ src/main/java/com/smicha/dev/workarea/WorkareaApplication.java
@@ -0,0 +1,13 @@
+package com.smicha.dev.workarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkareaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smicha/dev/workarea/WorkareaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smicha.dev.workarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkareaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
